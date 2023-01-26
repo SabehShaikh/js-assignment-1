26da0ae106594ff700533a00444c8dfc21b4b63c
SabehShaikh <121764329+SabehShaikh@users.noreply.github.com>
2023-01-27 00:05:04 +0500

    added html and javascript file.

---
+++ app.js
@@ -0,0 +1,32 @@
+// **** Chapter 1
+
+// question 1
+
+alert("Hello, Sir")
+
+// question 2
+
+alert("Error! Please enter a valid password");
+
+// question 3
+
+alert("Welcome to JS Land... \n Happy Coding")
+
+// question 4
+
+alert("Welcome to JS Land...")
+
+alert("Happy Coding!")
+
+// question 5
+
+alert("Hello... I can run JS through my web browser's console")
+
+// question 6
+ // i have made alert use in my existing html file
+
+ //question 7
+ //done as well
+
+
+
